770715a4c2c0d3a6b4c978400c808a07cb8dc856
Eduardo <eduardofcbg@gmail.com>
2021-01-14 10:45:26 +0000

    create default

---
+++ createdefault.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+usersfile=$1
+group=$2
+
+cat ./turmas/$usersfile | while read user 
+do
+  password="${user}2004${user}2005"	
+  label=$user
+  echo "Creating user $user with password $password"
+  echo $password | bash create.sh $group $label > /dev/null
+done
+
